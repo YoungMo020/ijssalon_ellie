96a5091efac9ba8c41792c1c25e85b3853b891b1
Ali Tanwir <mohammed.ali.tanwir@gmail.com>
2024-04-21 06:44:18 +0200

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -1,9 +1,7 @@
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
-
-print("Mijn favoriete smaak is aardbei".replace("aardbei", "citroen"))
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
